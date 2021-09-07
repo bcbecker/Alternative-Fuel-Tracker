087d5d31f3d3711895e8520cbf70f6f928962406
Bryce Becker <bryce.becker15@gmail.com>
2021-09-07 16:05:45 -0400

    Added form, query results
    Made form to query API for results, and display them

---
+++ fuel_tracker/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, SelectField
+from wtforms.validators import DataRequired
+
+
+class SearchForm(FlaskForm):
+    """
+    Collects parameters for querying API
+    """
+    fuel_type_choices = [("BD", "Biodiesel"), ("CNG", "Compressed Natural Gas"),
+            ("ELEC", "Electric"), ("E85", "Ethanol (E85)"), ("HY", "Hydrogen"),
+                ("LNG", "Liquefied Natural Gas"), ("LPG", "Propane")]
+    distance_radius_choices = ["1", "2", "3", "4", "5", "10", "15", "20", "25", "50", "100"]
+
+    address_city_state_zip = StringField("Search Location", validators=[DataRequired()])
+    fuel_type = SelectField("Fuel Type", choices=fuel_type_choices, validate_choice=True,\
+         default="all", validators=[DataRequired()])
+    distance_radius = SelectField("Distance", choices=distance_radius_choices, validate_choice=True,\
+        default="5", validators=[DataRequired()])
+    submit = SubmitField("Find Stations")
--- fuel_tracker/routes.py
+++ fuel_tracker/routes.py
@@ -1,23 +1,49 @@
-from flask import render_template, request, Blueprint
-#temporary, need to establish some other way
+from flask import render_template, url_for, redirect, request, Blueprint
 from config import Config
+from fuel_tracker.forms import SearchForm
 import requests
 import json
 
 main = Blueprint('main', __name__)
 
-#these need to go to a search function, data gathered from a form
-fuel_type = "E85"
-location = "48382"
-range_in_miles = "7"
-#limit or offset? (see docs)
 
-URL = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + str(Config.API_KEY) + "&location=" + location + "&fuel_type=" + fuel_type + "&radius=" + range_in_miles
-
-
-@main.route("/")
-@main.route("/home")
+@main.route("/", methods=['GET', 'POST'])
+@main.route("/home", methods=['GET', 'POST'])
 def home():
-    response = requests.get(URL).json()
-    location_response = response['fuel_stations']
-    return render_template('index.html', location_response=location_response)
+    """
+    Entry point, handles search form input/submission
+    """
+    form = SearchForm()
+    if form.validate_on_submit():
+        url = generate_url(form.address_city_state_zip.data, form.fuel_type.data, form.distance_radius.data)
+        response = requests.get(url).json()
+        results_found = response['total_results']
+        locations = response['fuel_stations']
+        return redirect(url_for('main.search', locations=locations, results_found=results_found , form=form))
+    return render_template('index.html', form = form)
+
+@main.route("/search", methods=['GET', 'POST'])
+def search():
+    """
+    Fetches search results, handles re-search form input/submission
+    """
+    form = SearchForm()
+    if form.validate_on_submit():
+        url = generate_url(form.address_city_state_zip.data, form.fuel_type.data, form.distance_radius.data)
+        response = requests.get(url).json()
+        results_found = response['total_results']
+        locations = response['fuel_stations']
+        return render_template('search_results.html', locations=locations, results_found=results_found, form=form)
+    return redirect(url_for('main.home', form = form))
+
+
+#Utility to create API query url
+def generate_url(location, fuel_type, range_in_miles):
+    """
+    Concats the search params to the API url
+    """
+    URL_WITH_KEY = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + str(Config.API_KEY)
+    #limit or offset? (see docs)
+    search_query = URL_WITH_KEY + "&location=" + location + "&fuel_type=" + fuel_type +\
+            "&radius=" + range_in_miles
+    return search_query
