5052b3307f42f04fc085cc1b3829e4ae90a881ea
Bryce Becker <bryce.becker15@gmail.com>
2021-09-15 18:57:00 -0400

    Updated config

--- config.py
+++ config.py
@@ -4,3 +4,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', "SECRET_KEY does not exist")
     NREL_API_KEY = os.environ.get('NREL_API_KEY', "API_KEY does not exist")
     GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', "API_KEY does not exist")
+    DEBUG = True
+    TESTING = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
